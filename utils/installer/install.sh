a8f348c09c872b019689ae59dc890bc6df947d41
ohinibla <ohinibla@gmail.com>
2025-08-28 07:11:03 +0330

    changing installer remote and config path

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -5,12 +5,12 @@ OS="$(uname -s)"
 
 #Set branch to master unless specified by the user
 declare -x LV_BRANCH="${LV_BRANCH:-"master"}"
-declare -xr LV_REMOTE="${LV_REMOTE:-lunarvim/lunarvim.git}"
+declare -xr LV_REMOTE="${LV_REMOTE:-ohinibla/lunarvim.git}"
 declare -xr INSTALL_PREFIX="${INSTALL_PREFIX:-"$HOME/.local"}"
 
 declare -xr XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
 declare -xr XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}"
-declare -xr XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+declare -xr XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/dotfiles/.config"}"
 
 declare -xr NVIM_APPNAME="${NVIM_APPNAME:-"lvim"}"
 
